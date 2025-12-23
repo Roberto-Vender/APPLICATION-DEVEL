79bf20290803fbf46836a6d6c7ad7dcdf6cb9ab2
Roberto-Vender <185998296+Roberto-Vender@users.noreply.github.com>
2025-12-23 11:11:55 +0800

    da

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { fetchWithAuth } from "../api";
 
 const Dashboard = () => {
   const [user, setUser] = useState(null);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { API_BASE } from "../api";
 
 const Login = () => {
   const [email, setEmail] = useState('');
